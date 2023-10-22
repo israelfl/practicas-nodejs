b0caef04aad9045fd578141ed4b399c118d27f56
Israel Falcón Leal <ifldeveloer@gmail.com>
2023-10-22 13:02:38 +0200

    Prettier y ESLint

--- 001-introduction-first-steps-modules/cjs/sum.js
+++ 001-introduction-first-steps-modules/cjs/sum.js
@@ -1,8 +1,8 @@
-function sum(a, b) {
-  return a + b;
+function sum (a, b) {
+  return a + b
 }
 
 // CommonJS Module Export
 module.exports = {
-  sum,
-};
+  sum
+}
--- 001-introduction-first-steps-modules/mjs/index.mjs
+++ 001-introduction-first-steps-modules/mjs/index.mjs
@@ -2,8 +2,8 @@
 // .mjs -> para utilizar ES Modules
 // .cjs -> para utilizar CommonJS
 
-import { sum, sub, mult } from "./sum.mjs";
+import { sum, sub, mult } from './sum.mjs'
 
-console.log(sum(1, 2));
-console.log(sub(1, 2));
-console.log(mult(1, 2));
+console.log(sum(1, 2))
+console.log(sub(1, 2))
+console.log(mult(1, 2))
